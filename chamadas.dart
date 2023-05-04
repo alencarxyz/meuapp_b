22a8ebdd50479e2e95722fc8307a3596abc64853
alencarxyz <127432388+alencarxyz@users.noreply.github.com>
2023-05-04 18:07:03 -0400

    Add files via upload

---
+++ chamadas.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart'; 
+class Chamadas  extends StatelessWidget {
+  const Chamadas ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      child: Text("Página de Chamadas"),
+    );
+  }
+}
