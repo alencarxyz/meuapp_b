000d7bd67d22833a91b92c700d1a0bc07d9aef56
alencarxyz <127432388+alencarxyz@users.noreply.github.com>
2023-08-01 14:10:53 -0400

    01/08/23

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,49 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
-
 import 'package:flutter/material.dart';
-import 'package:meuapp/pagina_inicial.dart';
-import 'configuracoes.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(PaginaInicial());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class PaginaInicial extends StatelessWidget {
+  const PaginaInicial({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      routes: {
-        '/':(context) => PaginaInicial(),
-        '/config':(context) => Configuracoes (),
-      },
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    String x = "100";
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Meu App"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(x),
+            ElevatedButton(
+              onPressed: () {
+                //AO PRESSIONAR O BOTAO
+                print("Antes de zerar");
+                print(x);
+                x = "0";
+                print("Depois de zerar");
+                print(x);
+              },
+              child: Text("Zerar"),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
